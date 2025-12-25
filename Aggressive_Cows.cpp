070fa74031ebe64cb5b536badc64bfce658951a5
Nenshiv <9.nency@gmail.com>
2025-12-25 16:49:11 +0530

    Refactor aggressive cows problem implementation

--- Aggressive_Cows.cpp
+++ Aggressive_Cows.cpp
@@ -1,46 +1,45 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
+    int stalls[] = {1, 2, 8, 4, 9};
+    int Cows = 3;
+    int totalStalls = 5;
 
-    int stall[] = {1,2,8,4,9};
-    int C = 3;
-    int n = 5;
-
-    int size = sizeof(stall) / sizeof(stall[0]);
-    for(int i = 0;i < size; i++) {
-        for(int j = i + 1;j < size; j++) {
-            if(stall[i] > stall[j]) {
-                swap(stall[i] , stall[j]);
+    // Sorting the stalls
+    for(int i = 0; i < totalStalls - 1; i++) {
+        for(int j = i + 1; j < totalStalls; j++) {
+            if(stalls[i] > stalls[j]) {
+                swap(stalls[i], stalls[j]);
             }
         }
     }
-    // for(int k = 0; k < size; k++) {
-    //     cout << stall[k] << " ";
-    // }
+
     int low = 0;
-    int high = stall[size - 1] - stall[0];
+    int high = stalls[totalStalls - 1] - stalls[0];
     int ans = 0;
-    
+
     while(low <= high) {
         int mid = low + (high - low) / 2;
         int count = 1;
-        int lastPos = stall[0];
-        for(int i = 0; i < n; i++) {
-            if(stall[i] - lastPos >= mid) {
+        int lastPos = stalls[0];
+
+        for(int i = 1; i < totalStalls; i++) {
+            if(stalls[i] - lastPos >= mid) {
                 count++;
-                lastPos = stall[i];
+                lastPos = stalls[i];
             }
         }
 
-            if(count >= C) {
-                ans = mid;
-                low = mid + 1;
-            }
-            else {
-                high = mid - 1;
-            }
+        if(count >= Cows) {
+            ans = mid;
+            low = mid + 1;
+        } else {
+            high = mid - 1;
         }
-    cout << ans << " ";
+    }
+
+    cout << ans << endl;
     return 0;
 }
+
